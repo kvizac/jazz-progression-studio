e3e641da3821a7b0bfeb536a1889edf6224883d3
kvizac <43444216+kvizac@users.noreply.github.com>
2026-09-08 02:19:20 +0200

    Add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    target: 'es2022',
+    sourcemap: true,
+  },
+  test: {
+    environment: 'node',
+  },
+});
